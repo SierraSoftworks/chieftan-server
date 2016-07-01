1ae7b2509aea0eb743358f98cded8c460843e4b6
Benjamin Pannell <admin@sierrasoftworks.com>
2016-07-01 10:59:10 +0200

    feat: Automatically initialize DB indexes on connection

--- models/Database.ts
+++ models/Database.ts
@@ -4,7 +4,17 @@ import {Project, ProjectDoc} from "./Project";
 import {Task, TaskDoc} from "./Task";
 
 export class Database extends Iridium.Core {
-    Actions = new Iridium.Model<ActionDoc, Action>(this, Action);
-    Projects = new Iridium.Model<ProjectDoc, Project>(this, Project);
-    Tasks = new Iridium.Model<TaskDoc, Task>(this, Task);
+    Actions: Iridium.Model<ActionDoc, Action> = new Iridium.Model<ActionDoc, Action>(this, Action);
+    Projects: Iridium.Model<ProjectDoc, Project> = new Iridium.Model<ProjectDoc, Project>(this, Project);
+    Tasks: Iridium.Model<TaskDoc, Task> = new Iridium.Model<TaskDoc, Task>(this, Task);
+
+    onConnected() {
+        return super.onConnected().then(() => Promise.all([
+            this.Actions.ensureIndexes(),
+            this.Projects.ensureIndexes(),
+            this.Tasks.ensureIndexes(),
+        ])).then(() => {
+            
+        });
+    }
 }
