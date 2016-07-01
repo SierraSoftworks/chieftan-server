57e4ab7f9f38fcf83918a437a8f115d3820763d9
Benjamin Pannell <admin@sierrasoftworks.com>
2016-07-01 14:31:11 +0200

    feat: Allow requesting a specific audit log

--- routes/auditLog.ts
+++ routes/auditLog.ts
@@ -4,9 +4,17 @@ import {assign, pick} from "lodash";
 
 export class AuditLog extends RouteBase {
     register() {
-        this.server.get("/api/v1/audit", this.authorize(), (req, res) => {
+        this.server.get("/api/v1/audit", this.authorize(), this.permission("admin"), (req, res) => {
             this.db.AuditLog.find().sort({ timestamp: -1 }).toArray().then(logs => {
-                res.send(200, logs);
+                res.send(logs);
+            }, err => this.databaseError(err)).catch(err => this.catch(res, err));
+        });
+
+        this.server.get("/api/v1/audit/:id", this.authorize(), this.permission("admin"), (req, res) => {
+            this.db.AuditLog.get(req.params.id).then(log => {
+                if (!log) return this.notFound();
+
+                res.send(log);
             }, err => this.databaseError(err)).catch(err => this.catch(res, err));
         });
     }
--- test/api.auditLog.ts
+++ test/api.auditLog.ts
@@ -8,18 +8,62 @@ describe("api", () => {
         before(() => app.setup());
         after(() => app.teardown());
 
+        let auditLogID: string;
+        before(() => app.db.AuditLog.insert({
+            type: "test",
+            user: app.testUser.summary,
+            context: {
+
+            }
+        }).then(log => auditLogID = log._id));
+
         it("should allow you to get the audit log", () => {
             return request(app.server)
                 .get("/api/v1/audit")
-                .set("Authorization", "Token test")
+                .set("Authorization", "Token admin")
                 .expect(200)
-                .toPromise();
+                .toPromise()
+                .then(res => {
+                    chai.expect(res.body).to.exist;
+                    chai.expect(res.body).to.have.length(1);
+                });
         });
 
         it("should return a 401 if you aren't authorized", () => {
             return request(app.server)
                 .get("/api/v1/audit")
                 .expect(401)
+                .toPromise();
+        });
+
+        it("should return a 403 if you aren't an admin", () => {
+            return request(app.server)
+                .get("/api/v1/audit")
+                .set("Authorization", "Token test")
+                .expect(403)
+                .toPromise();
+        });
+
+        it("should allow you to get a specific audit log entry", () => {
+            return request(app.server)
+                .get(`/api/v1/audit/${auditLogID}`)
+                .set("Authorization", "Token admin")
+                .expect(200)
+                .toPromise();
+        });
+
+        it("should return a 401 if you aren't authorized", () => {
+            return request(app.server)
+                .get(`/api/v1/audit/${auditLogID}`)
+                .expect(401)
+                .toPromise();
+        });
+
+        it("should return a 403 if you aren't an admin", () => {
+            return request(app.server)
+                .get(`/api/v1/audit/${auditLogID}`)
+                .set("Authorization", "Token test")
+                .expect(403)
                 .toPromise();
         });
     });
