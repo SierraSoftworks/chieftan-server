e81c0683fc460b85f791be68972f1b65d1897b86
Benjamin Pannell <admin@sierrasoftworks.com>
2016-08-08 17:24:48 +0200

    test: Add tests for createUser task

---
+++ src/tasks/createUser_test.go
@@ -0,0 +1,23 @@
+package tasks
+
+import . "gopkg.in/check.v1"
+
+func (s *TasksSuite) TestCreateUser(c *C) {
+	req := &CreateUserRequest{
+		Name:  "Test User",
+		Email: "test@test.com",
+		Permissions: []string{
+			"admin",
+		},
+	}
+
+	user, err := CreateUser(req)
+	c.Assert(err, IsNil)
+	c.Assert(user, NotNil)
+
+	c.Check(user.ID, Equals, "b642b4217b34b1e8d3bd915fc65c4452")
+	c.Check(user.Name, Equals, "Test User")
+	c.Check(user.Email, Equals, "test@test.com")
+	c.Check(user.Permissions, DeepEquals, []string{"admin"})
+	c.Check(user.Tokens, DeepEquals, []string{})
+}
