b4a034fcac1359fdae1deba97546f605af9f702d
Benjamin Pannell <admin@sierrasoftworks.com>
2016-08-10 08:36:42 +0200

    tweak: Don't include user tokens in the user list

--- src/tasks/getUsers.go
+++ src/tasks/getUsers.go
@@ -10,7 +10,9 @@ type GetUsersRequest struct {
 
 func GetUsers(req *GetUsersRequest) ([]models.User, error) {
 	var users []models.User
-	if err := models.DB().Users().Find(&bson.M{}).All(&users); err != nil {
+	if err := models.DB().Users().Find(&bson.M{}).Select(&bson.M{
+		"tokens": 0,
+	}).All(&users); err != nil {
 		return nil, NewError(500, "Server Error", "We encountered an error retrieving the users list from the database.")
 	}
 
