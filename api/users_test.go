2b9c68201b710343445831de8e854fb5a5203034
Benjamin Pannell <admin@sierrasoftworks.com>
2016-08-23 11:52:21 +0200

    test: Add tests for getting a specific user

--- api/users_test.go
+++ api/users_test.go
@@ -66,5 +66,52 @@ func (s *UsersAPISuite) TestGetUsers(c *C) {
 	res, err = client.Do(req)
 	defer res.Body.Close()
 	c.Assert(err, IsNil)
+	c.Check(res.StatusCode, Equals, 200)
+}
+
+func (s *UsersAPISuite) TestGetUser(c *C) {
+	ts := httptest.NewServer(Router())
+	defer ts.Close()
+
+	user, err := tasks.CreateUser(&tasks.CreateUserRequest{
+		Name:  "Test User",
+		Email: "test@test.com",
+	})
+
+	url := fmt.Sprintf("%s%s%s", ts.URL, "/v1/user/", user.ID)
+
+	c.Assert(err, IsNil)
+	c.Assert(user, NotNil)
+
+	req, err := http.NewRequest("GET", url, nil)
+	c.Assert(err, IsNil)
+
+	client := &http.Client{}
+
+	res, err := client.Do(req)
+	defer res.Body.Close()
+	c.Assert(err, IsNil)
+	c.Assert(res.StatusCode, Equals, 401)
+
+	token, err := tasks.CreateToken(&tasks.CreateTokenRequest{
+		UserID: user.ID,
+	})
+	c.Assert(err, IsNil)
+	c.Assert(token, Not(Equals), "")
+
+	req.Header.Add("Authorization", fmt.Sprintf("Token %s", token))
+
+	res, err = client.Do(req)
+	defer res.Body.Close()
+	c.Assert(err, IsNil)
+	c.Assert(res.StatusCode, Equals, 403)
+
+	user, err = tasks.SetPermissions(&tasks.SetPermissionsRequest{
+		UserID:      user.ID,
+		Permissions: []string{"admin/users"},
+	})
+	res, err = client.Do(req)
+	defer res.Body.Close()
+	c.Assert(err, IsNil)
 	c.Assert(res.StatusCode, Equals, 200)
 }
